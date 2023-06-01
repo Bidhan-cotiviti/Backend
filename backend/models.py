426240e818bbc1f8c7588915f9a166c8492f9234
Khatiwada <bidhan.khatiwada@cotiviti.com>
2023-06-01 11:41:14 +0545

    Date error fixed

--- backend/models.py
+++ backend/models.py
@@ -4,17 +4,18 @@ class RD(models.Model):
     member = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
     remarks = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.position} ({self.created_at})"
+    # def __str__(self):
+    #     return f" Member: {self.member}, Position: {self.position}, Remarks: {self.remarks}, Date: {self.date}"
     
 
 class DB(models.Model):
     member = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
     remarks = models.TextField()
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"{self.position} ({self.created_at})"
+    # def __str__(self):
+    #     return f" Member: {self.member}, Position: {self.position}, Remarks: {self.remarks}, Date: {self.date}"
+
